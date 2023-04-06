494e1f5f12bdba584a30de77740099c8b59f46ad
Hassan Sambo <sambohasan@gmail.com>
2023-04-06 23:12:43 +0100

    update jwt auth helper

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -1,3 +1,6 @@
+from flask import request
+import json
+
 ## AuthError Exception
 """
 AuthError Exception
@@ -9,3 +12,39 @@ class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
+
+
+def get_token_auth_header():
+    """Function to get access token from Authorization hearder"""
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError(
+            {
+                "code": "authorization_header_missing",
+                "description": "Authorization header is expected",
+            },
+            401,
+        )
+    parts = auth.split()
+    if parts[0].lower() != "bearer":
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Authorization header must start with 'Bearer'.",
+            },
+            401,
+        )
+    elif len(parts) == 1:
+        raise AuthError(
+            {"code": "invalid_header", "description": "Token not found"}, 401
+        )
+    elif len(parts) > 2:
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Authorization header must be bearer token",
+            },
+            401,
+        )
+    access_token = parts[1]
+    return access_token
